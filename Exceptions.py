c608a9e3781e4b24a371eab2d80fe246d7ea345d
Nathan Lintz <Nlintz@gmail.com>
2012-12-11 11:46:37 -0500

    added abstract model and updated PM

--- Exceptions.py
+++ Exceptions.py
@@ -12,5 +12,11 @@ class MultipleModelException(Exception):
         return "A model with the same name is already in \
         the database, rename the model you are trying to save"
 
+
+class NoInstanceException(Exception):
+    def __str__(self):
+        return "There is no model instance with this instanceName \
+        in the database"
+
 if __name__ == "__main__":
     raise MultipleInstanceException()
--- PickleMonger.py
+++ PickleMonger.py
@@ -1,64 +1,101 @@
 import pickle
 from Exceptions import *
-# DBFILE is a constant which names the file ObjectMap is read from
-DBFILENAME = ".pickleDB.dat"
+# TODO, abstract model, editmodelname, testcode, abstract open+close methods
+#DBFILENAME = ".pickleDB.dat"
 
 
 class PickleMonger(object):
 
     #The constructor creates a database file and an object map
-    def __init__(self, objectMap={}):
-        dbFile = open(DBFILENAME, 'w+b')
+    def __init__(self, DBFILENAME, objectMap={}):
+        self.DBFILENAME = DBFILENAME
+        dbFile = open(self.DBFILENAME, 'a+b')
         self.objectMap = objectMap
         pickle.dump(self.objectMap, dbFile)
         dbFile.close()
 
     #This method is used for adding new models to the database
     def create(self, modelname):
-        dbFile = open(DBFILENAME, 'r+b')
+        dbFile = open(self.DBFILENAME, 'r+b')
         currentObjectMap = pickle.load(dbFile)
         if modelname in currentObjectMap.keys():
             raise MultipleModelException
         self.objectMap[modelname] = []
-        dbFile = open(DBFILENAME, 'w+b')
+        dbFile = open(self.DBFILENAME, 'w+b')
         pickle.dump(self.objectMap, dbFile)
         dbFile.close()
 
     #This method returns the objects stored in a model
-    def read(self, modelName):
-        dbFile = open(DBFILENAME, 'r+b')
+    def read_allInstances(self, modelName):
+        dbFile = open(self.DBFILENAME, 'r+b')
         self.objectMap = pickle.load(dbFile)
         return self.objectMap[modelName]
         dbFile.close()
 
+    def read_oneInstances(self, modelName, instanceName):
+        dbFile = open(self.DBFILENAME, 'r+b')
+        self.objectMap = pickle.load(dbFile)
+        for instance in self.objectMap:
+            if instance.instanceName == instanceName:
+                return instance
+        dbFile.close()
+        raise NoInstanceException
+
     #This method is used to add objects to a model
     def addObject(self, model):
         modelName = model.modelName
-        dbFile = open(DBFILENAME, 'r+b')
+        dbFile = open(self.DBFILENAME, 'r+b')
         self.objectMap = pickle.load(dbFile)
+
         for instances in self.objectMap[model.modelName]:
             if instances.instanceName == model.instanceName:
                 raise MultipleInstanceException()
         self.objectMap[modelName].append(model)
-        dbFile = open(DBFILENAME, 'w+b')
+        dbFile = open(self.DBFILENAME, 'w+b')
         pickle.dump(self.objectMap, dbFile)
         dbFile.close()
 
     #This method destroys a model
     def destroyModel(self, modelName):
-        dbFile = open(DBFILENAME, 'r+b')
+        dbFile = open(self.DBFILENAME, 'r+b')
         self.objectMap = pickle.load(dbFile)
-        self.objectMap.pop(modelName)
+        for k, v in self.objectMap:
+            if k == modelName:
+                self.objectMap.pop(modelName)
         dbFile.close()
+        raise NoInstanceException
 
     #This method destroys an object in a model
     def destroyObjectInstance(self, modelName, instanceName):
-        dbFile = open(DBFILENAME, 'r+b')
+        dbFile = open(self.DBFILENAME, 'r+b')
         self.objectMap = pickle.load(dbFile)
         for modelObject in self.objectMap[modelName]:
             if modelObject.instanceName == instanceName:
                 self.objectMap[modelName].remove(modelObject)
         dbFile.close()
+
+    def editInstance(self, modelName, instanceName, newInstance):
+        dbFile = open(self.DBFILENAME, 'r+b')
+        self.objectMap = pickle.load(dbFile)
+        for instance in self.objectMap[modelName]:
+            if instance.instanceName == instanceName:
+                self.objectMap[modelName][instance] = newInstance
+
+        dbFile = open(self.DBFILENAME, 'w+b')
+        pickle.dump(self.objectMap, dbFile)
+        dbFile.close()
+
+
+
+    def editModelName(self, modelName, newModelName):
+        dbFile = open(self.DBFILENAME, 'r+b')
+        self.objectMap = pickle.load(dbFile)
+        self.objectMap[newModelName] = self.objectMap[modelName]
+        del self.objectMap[modelName]
+        dbFile = open(self.DBFILENAME, 'w+b')
+        pickle.dump(self.objectMap, dbFile)
+        dbFile.close()
+
 
 
 
@@ -73,7 +110,7 @@ class PickleMonger(object):
 
 
 #if __name__=="__main__":
-    #PM = PickleMonger()
+    #PM = PickleMonger('PICKLE3.db')
     #t = Model('nathan','n1','12345')
     #t2 = Model('nathan','n2','2343')
     #t3 = Model('nathan','n3','ppp')
@@ -81,3 +118,4 @@ class PickleMonger(object):
     #PM.addObject(t)
     #PM.addObject(t2)
     #PM.destroyObjectInstance('nathan','n1')
+
